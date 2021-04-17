ac8454df86107788865593b4936d048614301344
mzaini30 <muhzaini30@gmail.com>
2021-04-17 14:03:00 +0800

    upload

--- modul/olah-data.js
+++ modul/olah-data.js
@@ -7,3 +7,10 @@ let google = fs.readFileSync('./google.txt', 'utf8')
 
 let blog = fs.readFileSync('../blog.txt', 'utf8')
 blog = blog.split('\n')
+
+let baru = []
+for (let x of blog){
+	baru = [...baru, google.replace(/{url}/g, x)]
+}
+baru = baru.join('\n')
+console.log(baru)
