5cd41fe1629ed71386ee1bba380a460abc18cb5b
mzaini30 <muhzaini30@gmail.com>
2021-04-18 14:18:47 +0800

    upload

--- modul/rosemont.js
+++ modul/rosemont.js
@@ -7,5 +7,6 @@ export async function rosemont(){
 	// for (let x of kumpulan){
 		const x = 'https://google.com https://google.com https://google.com '
 		await driver.get('https://iway.rosemont.edu/ICS/Campus_Life/Campus_Groups/Organization_of_African_American_Students/Home.jnz?portlet=Discussion_Board&screen=PostView&screenType=change&id=04e9e807-e896-4946-ba8a-fe3d7a1d2d76&p=551')
+		await driver.executeScript(`document.querySelector('.btn.btn-sm.btn-primary.float-right').click()`)
 	// }
 }
